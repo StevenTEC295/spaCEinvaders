b907088c64a449aec9604a96aea950158c276510
StevenProgrammer01 <s.perez.3@estudiantec.cr>
2026-05-20 23:43:25 -0600

    Clase Jugador y Clase principal

--- spaceInvadersServer/src/spaceinvadersserver/model/Jugador.java
+++ spaceInvadersServer/src/spaceinvadersserver/model/Jugador.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package spaceinvadersserver.model;
 
-/**
- *
- * @author steve
- */
 public class Jugador {
+    private String jugadorId;
+    private int cannonX;
+    private int vidas;
+    private int puntos;
+    
+    public Jugador(String id, int cannonX){
+        this.jugadorId = id;
+        this.cannonX = cannonX;
+        this.vidas = 3;
+        this.puntos = 0;
+    
+    }
+    
+    public void moverDerecha(int velocidad, int maxX){
+        cannonX = Math.max(maxX, cannonX + velocidad);
+    }
+    public void moverIzquierda(int velocidad, int maxX){
+        cannonX = Math.min(maxX, cannonX + velocidad);
+    }
+    public void impacto () {vidas--;}
+    
+    public void agregarPuntos(int pts){puntos+= pts;}
+    
+    public boolean isMuerto(){return vidas <= 0;}
+    
+    // getters...
+    public String getJugadorId() { return jugadorId; }
+    public int getCannonX()     { return cannonX; }
+    public int getVidas()       { return vidas; }
+    public int getPuntos()       { return puntos; }
     
 }
